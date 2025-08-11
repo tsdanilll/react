0afe3fa37e1a3641437b7f9c0f6e86d645340068
Daniil <danil.tsoy@gmail.com>
2025-08-11 19:24:16 +0300

    playing around

--- tests.js
+++ tests.js
@@ -1,4 +1,10 @@
+let todos = [
+    {text: "learnJS", done: false},
+    {text: "drinkCoffee", done:true}
+];
 
-let age = 20;
-const name = "Daniil";
-isStudent = true;
+console.log(todos[0].text);
+
+todos.push({text: "Build a React app", done: false});
+
+console.log(todos[2]);
